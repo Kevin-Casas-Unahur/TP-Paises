b30e4a5f49456bcd4de05ea6171f07b13c53a417
NachoFzh <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2022-06-30 18:03:12 -0300

    arreglando un poco los errores

--- src/main/kotlin/tp.ws.kts
+++ src/main/kotlin/tp.ws.kts
@@ -8,27 +8,27 @@ interface PaisInter{
     var continente: String
     var codigoMonedaLocal: Int
     fun cotizacionNacionalDolar():Int
-    var paisesLimitrofes: MutableList<Envio>
-    var bloquesRegionales: MutableList<Envio>
-    var idiomasOficiales: MutableList<Envio>
+    var paisesLimitrofes: MutableList<PaisInter>
+    var bloquesRegionales: MutableList<Bloque>
+    var idiomasOficiales: MutableList<String>
 
-    fun esPlurinacional(){
-        return this.idiomasOficiales.size >=2
+    fun esPlurinacional(): Boolean {
+        return this.idiomasOficiales.size >= 2
     }
 
-    fun esIsla(){
+    fun esIsla(): Boolean {
         return this.paisesLimitrofes.size == 0
     }
 
-    fun densidadPoblacional(){
+    fun densidadPoblacional(): Int {
         return (this.poblacion / this.superficie).toInt()
     }
 
-    fun vecinoMasPoblado():Pais{
-        //el maximo en poblacion de los vecinos, pero cuenta también al pais local
-    }
+    // fun vecinoMasPoblado():Pais{
+    // el maximo en poblacion de los vecinos, pero cuenta también al pais local
+    // }
 
-    fun esLimitrofeDe(pais: Pais){
+    fun esLimitrofeDe(pais: Pais): Boolean {
         return this.paisesLimitrofes.contains(pais)
     }
 
@@ -43,4 +43,13 @@ interface PaisInter{
 
     fun aCuantoEquivale(pais: Pais){
     }
+}
+
+abstract class Bloque{
+    var paisesQueLoComponen: MutableList<PaisInter>
+
+}
+
+abstract class Pais: PaisInter{
+
 }
