640196e040d08249fd90f49c2732ae19f95d1b78
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2022-07-24 17:45:45 -0300

    agrego algunos test

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/PaisesTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/PaisesTest.kt
@@ -11,11 +11,11 @@ class PaisesTest: DescribeSpec ({
 
     val argentina = Pais("Argentina", "ARG", 45380000, 2780000.0, "America", "SRA", 128.22)
 
-    val brasil = Pais("Brasil", "BRA", 212600000, 851600000.0, "America", "BRL", 5.40)
+    val brasil = Pais("Brasil", "BRA", 212600000, 8516000.0, "America", "BRL", 5.40)
 
     val uruguay = Pais("Uruguay", "URY", 3474000, 176215.0, "America", "UYU", 41.09)
 
-    val bolivia = Pais("Bolivia", "BOL", 11670000, 1099000000.0, "America", "BOB", 6.88)
+    val bolivia = Pais("Bolivia", "BOL", 11670000, 1099000.0, "America", "BOB", 6.88)
 
     val cuba = Pais("Cuba", "CUB", 11330000, 113860.0, "America", "CUP", 23.96)
 
@@ -96,8 +96,24 @@ class PaisesTest: DescribeSpec ({
         argentina.aCuantoEquivale(brasil, 100.0).shouldBe(4.21)
     }
 
-    describe("Codigo alfa 3 de argentina"){
+    describe("Codigo alfa 3"){
         argentina.codigoAlfa3.shouldBe("ARG")
+        bolivia.codigoAlfa3.shouldBe("BOL")
+        uruguay.codigoAlfa3.shouldBe("URY")
     }
+
+    describe("codigo moneda local"){
+        argentina.codigoMonedaLocal.shouldBe("ARG")
+        bolivia.codigoMonedaLocal.shouldBe("BOL")
+        uruguay.codigoMonedaLocal.shouldBe("URY")
+    }
+
+    describe("densidad poblacional"){
+        argentina.densidadPoblacional().shouldBe(16)
+        bolivia.densidadPoblacional().shouldBe(10)
+        uruguay.densidadPoblacional().shouldBe(19)
+
+    }
+
 
 })
