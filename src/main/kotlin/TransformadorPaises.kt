351dd1a60540f64a94fa2a432720e2ea46ff950a
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2022-07-31 11:10:06 -0300

    Inicializacion de TransformadorPaises.kt

---
+++ src/main/kotlin/TransformadorPaises.kt
@@ -0,0 +1,11 @@
+import ar.edu.unahur.obj2.impostoresPaises.api.Country
+
+object TransformadorPaises {
+
+    fun transformarColleccion(colleccion: List<Country>) {
+        var elemTransformados: List<Pais>
+
+
+    }
+
+}
